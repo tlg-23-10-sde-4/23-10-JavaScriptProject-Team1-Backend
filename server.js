aa36f1f62e4503c613eabc2318622bcde1bc1bc6
Oshay Jackson <oshay@OSHAY.local>
2023-12-19 14:56:42 -0800

    server things

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require("express");
-
+const sequelize = require("./connection/connection.js");
 const cors = require("cors");
-
+const routes = require("./controller");
 const corsoption = {
-    origin: "http://localhost:3000"
-}
-
-const routes = require("./controller")
+  origin: "http://localhost:3000",
+};
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors(corsoption))
+app.use(cors(corsoption));
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(PORT, async () => {
-    console.log(`Now listening on port: ${PORT}`);
-}); 
+const startServer = async () => {
+  try {
+    // Ensure the database connection is established before starting the server
+    await sequelize.authenticate();
+    console.log("Connection to the database has been established successfully.");
 
+    // await sequelize.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Now listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+// Call the function to start the server
+startServer();
