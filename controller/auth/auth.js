237f1f36a209fc23c19b1dd44061fd031faa6ade
Oshay Jackson <oshay@OSHAY.local>
2023-12-19 13:50:35 -0800

    created signUp post route

--- controller/auth/auth.js
+++ controller/auth/auth.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 
 router.post("/login", async (req, res) => {
-    const {userName, password} = req.body;
-    console.log(userName);
-    console.log(password);
-    return res.status(200).json({message: "message received, MF"})
-
+  const { userName, password } = req.body;
+  console.log(userName);
+  console.log(password);
+  return res.status(200).json({ message: "message received, MF" });
 });
 
+// POST /signUp
+router.post("/signup", async (req, res) => {
+  const { email, username, password } = req.body;
+  console.table(email, username, password);
+  return res.status(200).json({ message: "congrats, you're KGB Kon-Bon Gamer" });
+});
 
 module.exports = router;
