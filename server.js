17235e4f93da334580736a6c4bea226324159a8b
jaytaylor-dev <javaris.taylor@tlgcohort.com>
2023-12-19 12:41:58 -0800

    addressed cors issue

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 
+const cors = require("cors");
+
+const corsoption = {
+    origin: "http://localhost:3000"
+}
+
 const routes = require("./controller")
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors(corsoption))
 
 app.use(express.json());
 
