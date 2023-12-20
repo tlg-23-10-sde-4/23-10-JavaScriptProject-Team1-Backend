3dff2881b5c777188e21f78b27bf058c155dd9c5
Oshay Jackson <oshay@OSHAY.local>
2023-12-19 16:39:14 -0800

    Signed-off-by: Oshay Jackson <oshay@OSHAY.local>

--- connection/connection.js
+++ connection/connection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
@@ -9,20 +8,3 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
-=======
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, 
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
-
-module.exports = sequelize;
->>>>>>> d1ae341084aa4b73b2345e577e44349876c5525a
