0f252c6c7b2e999343ebc9441df7de96e87f76c5
jaytaylor-dev <javaris.taylor@tlgcohort.com>
2023-12-20 15:03:12 -0800

    Got post get delete routes  done for comments

--- controller/index.js
+++ controller/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const authRoutes = require("./auth/auth.js");
 const gameRoutes = require('./game/game.js');
+const commentRoutes = require('./game/comment.js')
 
 router.use("/auth", authRoutes);
 router.use('/games', gameRoutes);
+router.use('/comment', commentRoutes)
 
 module.exports = router;
