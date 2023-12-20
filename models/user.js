8061b914159c14ce553c95de1a5856035c6ee693
Oshay Jackson <oshay@OSHAY.local>
2023-12-20 10:38:05 -0800

    user.js added routes and likes/dislikes to user object and pass isValidPassword

--- models/user.js
+++ models/user.js
@@ -1,36 +1,58 @@
-const { Model, Datatypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
-  checkPassword(LoginPassword) {
-    return bcrypt.compareSync(LoginPassword, this.password);
+  checkPassword(loginPassword) {
+    return bcrypt.compareSync(loginPassword, this.password);
   }
 }
 User.init(
   {
+    //! USER.ID
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    //! USER.USERNAME
     username: {
       type: DataTypes.STRING,
       allowNull: false,
+      defaultValue: "exmampleUsername",
     },
+    //! USER.EMAIL
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      defaultValue: "email@example.com",
       validate: {
         isEmail: true,
       },
     },
+    //! USER.PASSWORD
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [8],
+      },
+    },
+    //! USER.LIKES
+    likes: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      validate: {
+        len: [1],
+      },
+    },
+    //! USER.DISLIKES
+    dislikes: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      validate: {
+        len: [1],
       },
     },
   },
@@ -41,12 +63,22 @@ User.init(
         return newUserData;
       },
     },
-    sequelize,
+    sequelize: Sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: "user",
   }
 );
+//! creating user
+await User.create({ email: "oshay@email.com", password: "password" });
+//! finding the user
+const user = await User.findOne({
+  where: {
+    email: "oshay@email.com",
+  },
+});
+const isValidPassword = user.checkPassword("password");
+console.log(`Is the password valid? ${isValidPassword}`);
 
 module.exports = User;
