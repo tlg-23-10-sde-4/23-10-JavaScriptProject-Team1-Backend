e5eafca65c2949e77ed91a345a6414b2b108a553
Oshay Jackson <oshay@OSHAY.local>
2023-12-19 16:14:23 -0800

    divergent and installed crypt pkg

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require("express");
-<<<<<<< HEAD
+
 const sequelize = require("./connection/connection.js");
 const cors = require("cors");
 const routes = require("./controller");
 const corsoption = {
   origin: "http://localhost:3000",
 };
-=======
-const sequelize = require('./connection/connection.js');
-const cors = require("cors");
-const routes = require("./controller")
-const corsoption = {
-    origin: "http://localhost:3000"
-}
->>>>>>> d1ae341084aa4b73b2345e577e44349876c5525a
 
 const PORT = process.env.PORT || 3001;
 
@@ -26,7 +18,6 @@ app.use(express.json());
 app.use(routes);
 
 const startServer = async () => {
-<<<<<<< HEAD
   try {
     // Ensure the database connection is established before starting the server
     await sequelize.authenticate();
@@ -44,22 +35,3 @@ const startServer = async () => {
 
 // Call the function to start the server
 startServer();
-=======
-    try {
-        // Ensure the database connection is established before starting the server
-        await sequelize.authenticate();
-        console.log('Connection to the database has been established successfully.');
-
-        // await sequelize.sync();
-
-        app.listen(PORT, () => {
-            console.log(`Now listening on port: ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-};
-
-// Call the function to start the server
-startServer();
->>>>>>> d1ae341084aa4b73b2345e577e44349876c5525a
