37e218f27667330f347ddd48513a6c863e4dbe74
SSPENELOPE <typoepping@gmail.com>
2023-12-21 12:27:54 -0500

    Update controller index

--- controller/index.js
+++ controller/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 const authRoutes = require("./auth/auth.js");
 const gameRoutes = require('./game/game.js');
-const commentRoutes = require('./game/comment.js')
+const commentRoutes = require('./game/comment.js');
+const apiRoutes = require('./game/api.js');
 
 router.use("/auth", authRoutes);
 router.use('/games', gameRoutes);
-router.use('/comment', commentRoutes)
+router.use('/comment', commentRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
