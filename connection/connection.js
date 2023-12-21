d2f387b2a56ea98b14b82c8829983e7af6537d73
SSPENELOPE <typoepping@gmail.com>
2023-12-21 18:01:50 -0500

    Update connection to work with jawsDB.

--- connection/connection.js
+++ connection/connection.js
@@ -1,10 +1,18 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+
+let sequelize;
+
+// Check if the app is running in a production environment (Heroku)
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use local development configuration
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
